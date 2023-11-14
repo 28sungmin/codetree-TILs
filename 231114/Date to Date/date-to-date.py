592b5518a11fb1de3e32bbeaedadab65881d22d5
28sungmin <115343559+28sungmin@users.noreply.github.com>
2023-11-14 22:04:35 +0900

    Title: Date to Date, Time: 153ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/Date to Date/date-to-date.py
+++ 231114/Date to Date/date-to-date.py
@@ -5,12 +5,12 @@ months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 big_days = 0
 small_days = 0
 
-for i in range(1, m2+1):
+for i in range(1, m2):
     big_days += months[i]
 big_days += d2
 
-for i in range(1, m1+1):
+for i in range(1, m1):
     small_days += months[i]
 small_days += d1
 
-print(big_days - small_days)
+print(big_days - small_days + 1)
