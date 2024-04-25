afb47a2b6955aa7806500cae1d0a3335e4239c9f
28sungmin <115343559+28sungmin@users.noreply.github.com>
2024-04-25 12:16:13 +0900

    Title: 시력 검사, Time: 148ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240425/시력 검사/eye-test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        double leye, reye;
+        leye = sc.nextDouble();
+        reye = sc.nextDouble();
+
+        if (leye >= 1.0 && reye >= 1.0)
+            System.out.println("High");
+        else if (leye >= 0.5 && reye >= 0.5)
+            System.out.println("Midde");
+        else 
+            System.out.println("Low");
+    }
+}
