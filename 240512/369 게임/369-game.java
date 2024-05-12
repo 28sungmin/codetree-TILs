1542f3e493231a52c6d0560ae1d50cb9f99a444c
28sungmin <115343559+28sungmin@users.noreply.github.com>
2024-05-12 18:24:30 +0900

    Title: 369 게임, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240512/369 게임/369-game.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n, shr, rem, i;
+        n = sc.nextInt();
+
+
+        i = 1;
+        while (i <= n) {
+            if (i < 10) {
+                if (i % 3 == 0)
+                    System.out.printf("%d ", 0);
+                else 
+                    System.out.printf("%d ", i);
+            }
+            else {
+                shr = i / 10;
+                rem = i % 10;
+                if (i % 3 == 0 || shr % 3 == 0 || rem % 3 == 0)
+                    System.out.printf("%d ", 0);
+                else 
+                    System.out.printf("%d ", i);
+            }
+            i++;
+        }
+    }
+}
