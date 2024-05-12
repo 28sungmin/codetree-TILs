2f8bb3cf117146999e9b87ea1d8aff5ea4094934
28sungmin <115343559+28sungmin@users.noreply.github.com>
2024-05-12 17:55:48 +0900

    Title: 100점 도달하기, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240512/100점 도달하기/reach-100-point.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i <= 100; i++) {
+            if (i >= 90) System.out.printf("%c ", 'A');
+            else if (i >= 80) System.out.printf("%c ", 'B');
+            else if (i >= 70) System.out.printf("%c ", 'C');
+            else if (i >= 60) System.out.printf("%c ", 'D');
+            else System.out.printf("%c ", 'F');
+        }
+    }
+}
