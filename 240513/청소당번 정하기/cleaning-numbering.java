90baa239f4fde8c157320f697c30fb7c52e97f03
28sungmin <115343559+28sungmin@users.noreply.github.com>
2024-05-13 20:58:52 +0900

    Title: 청소당번 정하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240513/청소당번 정하기/cleaning-numbering.java
+++ 240513/청소당번 정하기/cleaning-numbering.java
@@ -7,16 +7,9 @@ public class Main {
         int n = sc.nextInt();
         int cnt_2 = 0, cnt_3 = 0, cnt_12 = 0;
         for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                if (i % 3 == 0) cnt_3++;
-                else if (i % 12 == 0) cnt_12++;
-                else cnt_2++;
-            }
-            else if (i % 3 == 0) {
-                if (i % 12 == 0) cnt_12++;
-                else cnt_3++;
-            }
-            else if (i % 12 == 0) cnt_12++;
+            if (i % 12 == 0) cnt_12++;
+            else if (i % 6 == 0 || i % 3 == 0) cnt_3++;
+            else if (i % 2 == 0) cnt_2++;
         }
         System.out.printf("%d %d %d", cnt_2, cnt_3, cnt_12);
     }
