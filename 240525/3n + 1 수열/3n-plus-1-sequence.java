32106876c59e7a8befeb4c4806f660930e90776e
28sungmin <115343559+28sungmin@users.noreply.github.com>
2024-05-25 21:20:06 +0900

    Title: 3n + 1 수열, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240525/3n + 1 수열/3n-plus-1-sequence.java
+++ 240525/3n + 1 수열/3n-plus-1-sequence.java
@@ -9,6 +9,8 @@ public class Main {
         int cnt = 0;
 
         while (true) {
+            if (n == 1) break;
+            
             if (n % 2 == 0) {
                 n /= 2;
                 cnt++;
@@ -17,8 +19,6 @@ public class Main {
                 n = n * 3 + 1;
                 cnt++;
             }
-
-            if (n == 1) break;
         }
         System.out.print(cnt);
     }
