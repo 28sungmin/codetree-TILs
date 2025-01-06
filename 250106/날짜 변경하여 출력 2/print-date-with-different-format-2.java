87f2f7ae6d924ecdf4ca91b56346fe36a0853ea7
28sungmin <115343559+28sungmin@users.noreply.github.com>
2025-01-06 14:36:32 +0900

    Title: 날짜 변경하여 출력 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250106/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        scan.useDelimiter("-");
+
+        int m = scan.nextInt();
+        int d = scan.nextInt();
+        int y = scan.nextInt();
+
+        System.out.println(y + "." + m + "." + d);
+    }
+}
